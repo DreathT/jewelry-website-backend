5bc2fd22c9f458be5360d4ae57d763098eb67983
Abdullah Can <abdullahcan123gs@hotmail.com>
2025-07-07 13:33:40 +0300

    product controller created

---
+++ controllers/product.js
@@ -0,0 +1,26 @@
+import Product from "../models/product";
+import express from 'express';
+
+const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json(products);
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+const getProductById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findById(id);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        console.error("Error fetching product:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
