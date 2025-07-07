9bd5c45299e12f46e1e65b32320f5bdc69b04423
Abdullah Can <abdullahcan123gs@hotmail.com>
2025-07-07 19:58:03 +0300

    assert changed by fs

--- controllers/product.js
+++ controllers/product.js
@@ -1,5 +1,16 @@
-import products from "../data/products.json" assert { type: "json" };
 import { getGoldPrice } from "../utils/goldPriceApi.js";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Gerçek yolu doğru oluştur
+const productsPath = path.join(__dirname, "../data/products.json");
+
+const productsData = await fs.readFile(productsPath, "utf-8");
+const products = JSON.parse(productsData);
 
 const getAllProducts = async (req, res) => {
     try {
@@ -17,7 +28,7 @@ const getAllProducts = async (req, res) => {
         res.status(200).json(result);
 
     } catch (error) {
-        console.error("Error fetching products:", error);
+        console.error("Error fetching products:", error.message);
         res.status(500).json({ message: "Internal Server Error" });
     }
 }
